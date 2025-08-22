9e01056347685fd37946dc0907703107d83154de
Ijilusi Precious Ayomide <80537032+percie1@users.noreply.github.com>
2025-08-22 14:26:32 +0100

    Create my_second_script.sh

---
+++ my_second_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+clear
+echo "welcome to my second script"
+echo "i am just learnig, so stay with me"
+
+echo "your home directoty is: $HOME"
+
+echo "terminal type: $TERM"
+
+
+                                  
