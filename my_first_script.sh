0ec1d6e8b5e650ef65142ce95b8069ff9cdc0b0c
Ijilusi Precious Ayomide <80537032+percie1@users.noreply.github.com>
2025-08-22 14:25:25 +0100

    Create my_first_script.sh

---
+++ my_first_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+clear
+echo "welcome to my first script"
+
+echo "hi $USER!"
+echo
+
+echo "i will fetch you a list of a connected users:"
+echo
+
+echo "i am setting two variables now."
+COLOUR="black"
+VALUE="9"
+echo "this is a string: $COLOUR"
+echo "and this is  number: $VALUE"
+
+echo "i am giving you back your prompt now"
